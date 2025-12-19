7a7b359048f75cd47d0d31f8b4d17d1d87e489c9
Justin <justincrypto702@gmail.com>
2025-12-19 01:26:16 -0800

    Add function placeOrder

--- pizza-app/script.js
+++ pizza-app/script.js
@@ -10,5 +10,15 @@ const orderQueue = [];
 
 function addNewPizza (pizzaObj){
     menu.push(pizzaObj);
+    console.log(`new menu item: ${pizzaObj}`);
 }
 
+function placeOrder (name){
+    const orderedPizza = menu.find(pizzaObj => pizzaObj.name === name);
+    pizzaPrice = orderedPizza.price;
+     cashInRegister += pizzaPrice;
+    const newOrder = {pizza: orderedPizza, status: "ordered"};
+    orderQueue.push(newOrder);
+    console.log(`New placeOrder: ${name}`);
+    return newOrder;
+}
